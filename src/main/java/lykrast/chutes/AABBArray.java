1bf8661b5ab6e1936607dcf607ba706887499031
Lykrast <aymericpm@gmail.com>
2018-08-08 01:07:55 +0200

    Chutes now work

---
+++ src/main/java/lykrast/chutes/AABBArray.java
@@ -0,0 +1,272 @@
+package lykrast.chutes;
+
+/*
+MIT License
+
+Copyright (c) 2016-2018 BeetoGuy, primetoxinz, BordListian
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.Vec3d;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.function.Function;
+
+/**
+ * Hack class to allow multiple bounding boxes per entity.
+ *
+ * @author mrebhan
+ */
+
+public class AABBArray extends AxisAlignedBB {
+
+    private AxisAlignedBB[] boundingBoxes;
+
+    public AABBArray(AxisAlignedBB... boundingBoxes) {
+        this(join(boundingBoxes));
+        this.boundingBoxes = boundingBoxes;
+        this.boundingBoxes = getParts(this);
+    }
+
+    private AABBArray(AxisAlignedBB full) {
+        super(full.minX, full.minY, full.minZ, full.maxX, full.maxY, full.maxZ);
+    }
+
+    private static AxisAlignedBB join(AxisAlignedBB[] in) {
+        if (in.length == 0) {
+            return null;
+        }
+
+        AxisAlignedBB aabb1 = in[0];
+        for (int i = 1; i < in.length; i++) {
+            aabb1 = aabb1.union(in[i]);
+        }
+        return aabb1;
+    }
+
+    public static AxisAlignedBB[] getParts(AxisAlignedBB source) {
+        if (source instanceof AABBArray) {
+            HashSet<AxisAlignedBB> bbs = new HashSet<>();
+            Arrays.asList(((AABBArray) source).boundingBoxes)
+                    .forEach(aabb -> bbs.addAll(Arrays.asList(getParts(aabb))));
+            return bbs.toArray(new AxisAlignedBB[0]);
+        }
+        return new AxisAlignedBB[]{source};
+    }
+
+    public static AxisAlignedBB toAABB(AxisAlignedBB source) {
+        return new AxisAlignedBB(source.minX, source.minY, source.minZ, source.maxX, source.maxY, source.maxZ);
+    }
+
+    @Override
+    public boolean intersects(Vec3d p_189973_1_, Vec3d p_189973_2_) {
+        boolean flag = false;
+        for (AxisAlignedBB axisAlignedBB : boundingBoxes) {
+            flag |= axisAlignedBB.intersects(p_189973_1_, p_189973_2_);
+        }
+        return flag;
+    }
+
+    @Override
+    public boolean intersects(double x1, double y1, double z1, double x2, double y2, double z2) {
+        boolean flag = false;
+        for (AxisAlignedBB axisAlignedBB : getParts(this)) {
+            flag |= axisAlignedBB.intersects(x1, y1, z1, x2, y2, z2);
+        }
+        return flag;
+    }
+
+    public AABBArray addBoundingBox(AxisAlignedBB aabb) {
+        AxisAlignedBB[] bbs = new AxisAlignedBB[boundingBoxes.length + 1];
+        bbs[bbs.length] = aabb;
+        return new AABBArray(bbs);
+    }
+
+    @Override
+    public AABBArray offset(double x, double y, double z) {
+        AxisAlignedBB[] parts = getParts(this);
+        for (int i = 0; i < parts.length; i++) {
+            parts[i] = parts[i].offset(x, y, z);
+        }
+        return new AABBArray(parts);
+    }
+
+    @Override
+    public double calculateXOffset(AxisAlignedBB other, double offsetX) {
+        for (AxisAlignedBB axisAlignedBB : boundingBoxes) {
+            offsetX = axisAlignedBB.calculateXOffset(other, offsetX);
+        }
+        return offsetX;
+    }
+
+    @Override
+    public double calculateYOffset(AxisAlignedBB other, double offsetY) {
+        for (AxisAlignedBB axisAlignedBB : boundingBoxes) {
+            offsetY = axisAlignedBB.calculateYOffset(other, offsetY);
+        }
+        return offsetY;
+    }
+
+    @Override
+    public double calculateZOffset(AxisAlignedBB other, double offsetZ) {
+        for (AxisAlignedBB axisAlignedBB : boundingBoxes) {
+            offsetZ = axisAlignedBB.calculateZOffset(other, offsetZ);
+        }
+        return offsetZ;
+    }
+
+    public boolean intersectsWithXY(AxisAlignedBB other) {
+        boolean flag = false;
+        for (AxisAlignedBB axisAlignedBB : getParts(this)) {
+            flag |= axisAlignedBB.intersects(other.minX, other.minY, Double.NEGATIVE_INFINITY, other.maxX, other.maxY,
+                    Double.POSITIVE_INFINITY);
+        }
+        return flag;
+    }
+
+    public boolean intersectsWithXZ(AxisAlignedBB other) {
+        boolean flag = false;
+        for (AxisAlignedBB axisAlignedBB : getParts(this)) {
+            flag |= axisAlignedBB.intersects(other.minX, Double.NEGATIVE_INFINITY, other.minZ, other.maxX,
+                    Double.POSITIVE_INFINITY, other.maxZ);
+        }
+        return flag;
+    }
+
+    public boolean intersectsWithYZ(AxisAlignedBB other) {
+        boolean flag = false;
+        for (AxisAlignedBB axisAlignedBB : getParts(this)) {
+            flag |= axisAlignedBB.intersects(Double.NEGATIVE_INFINITY, other.minY, other.minZ, Double.POSITIVE_INFINITY,
+                    other.maxY, other.maxZ);
+        }
+        return flag;
+    }
+
+    @Override
+    public boolean intersects(AxisAlignedBB other) {
+        boolean flag = false;
+        for (AxisAlignedBB axisAlignedBB : boundingBoxes) {
+            flag |= axisAlignedBB.intersects(other);
+        }
+        return flag;
+    }
+
+    @Override
+    public boolean contains(Vec3d vec) {
+        boolean flag = false;
+        for (AxisAlignedBB axisAlignedBB : getParts(this)) {
+            flag |= axisAlignedBB.contains(vec);
+        }
+        return flag;
+    }
+
+    @Override
+    public boolean intersectsWithXY(Vec3d vec) {
+        boolean flag = false;
+        for (AxisAlignedBB axisAlignedBB : getParts(this)) {
+            flag |= axisAlignedBB.intersectsWithXY(vec);
+        }
+        return flag;
+    }
+
+    @Override
+    public boolean intersectsWithXZ(Vec3d vec) {
+        boolean flag = false;
+        for (AxisAlignedBB axisAlignedBB : getParts(this)) {
+            flag |= axisAlignedBB.intersectsWithXZ(vec);
+        }
+        return flag;
+    }
+
+    @Override
+    public boolean intersectsWithYZ(Vec3d vec) {
+        boolean flag = false;
+        for (AxisAlignedBB axisAlignedBB : getParts(this)) {
+            flag |= axisAlignedBB.intersectsWithYZ(vec);
+        }
+        return flag;
+    }
+
+    @Override
+    public AxisAlignedBB grow(double x, double y, double z) {
+        if (x == 0 && y == 0 && z == 0) {
+            return new AABBArray(boundingBoxes);
+        }
+        return super.grow(x, y, z);
+    }
+
+    @Override
+    public AxisAlignedBB grow(double value) {
+        return this.grow(value, value, value);
+    }
+
+    @Override
+    public RayTraceResult calculateIntercept(Vec3d vecA, Vec3d vecB) {
+        RayTraceResult result = null;
+        for (AxisAlignedBB axisAlignedBB : getParts(this)) {
+            result = axisAlignedBB.calculateIntercept(vecA, vecB);
+            if (result != null) {
+                break;
+            }
+        }
+        return result;
+    }
+
+    public AABBArray forEach(Function<AxisAlignedBB, AxisAlignedBB> consumer) {
+        AxisAlignedBB[] var1 = getParts(this);
+        for (int i = 0; i < var1.length; i++) {
+            var1[i] = consumer.apply(var1[i]);
+        }
+
+        return new AABBArray(var1);
+    }
+
+    // to implement
+
+    @Override
+    public AxisAlignedBB expand(double x, double y, double z) {
+        return super.expand(x, y, z);
+    }
+
+    @Override
+    public AxisAlignedBB shrink(double value) {
+        return super.shrink(value);
+    }
+
+    @Override
+    public AxisAlignedBB offset(BlockPos pos) {
+        return super.offset(pos);
+    }
+
+    @Override
+    public AxisAlignedBB setMaxY(double y2) {
+        return super.setMaxY(y2);
+    }
+
+    @Override
+    public AxisAlignedBB union(AxisAlignedBB other) {
+        return super.union(other);
+    }
+
+}
--- src/main/java/lykrast/chutes/BlockChute.java
+++ src/main/java/lykrast/chutes/BlockChute.java
@@ -1,16 +1,67 @@
 package lykrast.chutes;
 
+import java.util.List;
+
+import javax.annotation.Nullable;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.BlockFaceShape;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 
 public class BlockChute extends Block {
+	public static final AxisAlignedBB AABB_NORTH = new AxisAlignedBB(0, 0, 0, 1, 1, 0.0625);
+	public static final AxisAlignedBB AABB_SOUTH = new AxisAlignedBB(0, 0, 0.9375, 1, 1, 1);
+	public static final AxisAlignedBB AABB_WEST = new AxisAlignedBB(0, 0, 0, 0.0625, 1, 1);
+	public static final AxisAlignedBB AABB_EAST = new AxisAlignedBB(0.9375, 0, 0, 1, 1, 1);
+	public static final AxisAlignedBB CHUTE_AABB = new AABBArray(AABB_NORTH, AABB_SOUTH, AABB_WEST, AABB_EAST);
+	
 	public BlockChute(float hardness, float resistance) {
 		super(Material.WOOD);
 		setSoundType(SoundType.WOOD);
 		setHardness(hardness);
 		setResistance(resistance);
 		setHarvestLevel("axe", 0);
+	}
+
+	@Override
+	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+		return CHUTE_AABB;
+	}
+	
+	@Override
+    public void addCollisionBoxToList(IBlockState state, World worldIn, BlockPos pos, AxisAlignedBB entityBox, List<AxisAlignedBB> collidingBoxes, @Nullable Entity entityIn, boolean isActualState) {
+        addCollisionBoxToList(pos, entityBox, collidingBoxes, AABB_NORTH);
+        addCollisionBoxToList(pos, entityBox, collidingBoxes, AABB_SOUTH);
+        addCollisionBoxToList(pos, entityBox, collidingBoxes, AABB_WEST);
+        addCollisionBoxToList(pos, entityBox, collidingBoxes, AABB_EAST);
+    }
+
+	@Override
+    public boolean isTopSolid(IBlockState state) {
+        return false;
+    }
+
+	@Override
+	public boolean isFullCube(IBlockState state) {
+		return false;
+	}
+
+	@Override
+	public boolean isOpaqueCube(IBlockState state) {
+		return false;
+	}
+
+	@Override
+	public BlockFaceShape getBlockFaceShape(IBlockAccess worldIn, IBlockState state, BlockPos pos, EnumFacing face) {
+		return face == EnumFacing.UP || face == EnumFacing.DOWN ? BlockFaceShape.UNDEFINED : BlockFaceShape.SOLID;
 	}
 
 }
